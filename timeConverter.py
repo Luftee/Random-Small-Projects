9077cc89743e6bc83cba50f40a3f84765d437c08
Luft <69074182+Luftee@users.noreply.github.com>
2022-05-23 10:34:57 +0900

    Update timeConverter.py

--- timeConverter.py
+++ timeConverter.py
@@ -1,24 +1,26 @@
-def format_check(a):
+def format_check(Global a):
+    a = input()
     b = len(a)
     if b != 3 and b != 4:
         return True
-    elif int(a) > 2400:
+    elif int(a) > 2359:
         return True
 
 while True:
     print("Original timezone: ", end = "")
-    oldZone = input()
     if format_check(oldZone):
         break
     print("Original time: ", end = "")
-    oldTime = input()
     if format_check(oldTime):
         break
     print("New timezone: ", end = "")
-    newZone = input()
     if format_check(newZone):
         break
-
+        
+    newTime = int(oldTime) - int(oldZone) + int(newZone)
+    if newTime > 2359:
+        newTime = newTime - 2400
+    
     print("\nNew time: ", end = "")
-    print(int(oldTime) - int(oldZone) + int(newZone))
+    print(newTime)
     break
