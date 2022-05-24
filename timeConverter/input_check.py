cec9f9d1a21e8922512c4d6cbac26e5767b1e55d
Luft <69074182+Luftee@users.noreply.github.com>
2022-05-24 09:17:33 +0900

    Update input_check.py

--- timeConverter/input_check.py
+++ timeConverter/input_check.py
@@ -1,1 +1,13 @@
+def input_check():
+    a = input()
+    
+    def format_error(a):
+        if a:
+            print("\nFormat error")
+            return False
 
+    b = len(a)
+    format_error(b < 3 or b > 4)
+    format_error(int(a) > 2359)
+    format_error(int(str(a)[-2:]) >= 60)
+    return a
