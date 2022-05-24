80757471a5437de8df79abd4d28d524f0f39780f
Luft <69074182+Luftee@users.noreply.github.com>
2022-05-24 09:17:19 +0900

    Create timeConverter.py

---
+++ timeConverter/timeConverter.py
@@ -0,0 +1,19 @@
+while True:
+    print("Original timezone: ", end = "")
+    oldZone = input_check()
+    if oldZone == False: break
+    print("Original time: ", end = "")
+    oldTime = input_check()
+    if oldTime == False: break
+    print("New timezone: ", end = "")
+    newZone = input_check()
+    if newZone == False: break
+        
+    newTime = int(oldTime) - int(oldZone) + int(newZone)
+    if int(str(newTime)[-2:]) >= 60: newTime = newTime - 60 + 100
+    if newTime > 2359: newTime = newTime - 2400
+    
+    print("\nNew time: ", end = "")
+    if newTime < 1000: print("0", end = "")
+    print(newTime)
+    break
