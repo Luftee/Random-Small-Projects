dfef80678cf218b20ff3f954c95776ac56bcdf80
Luft <69074182+Luftee@users.noreply.github.com>
2022-05-26 11:04:38 +0900

    Update functions.py

--- timeConverter/functions.py
+++ timeConverter/functions.py
@@ -11,3 +11,6 @@ def input_check(b):
     format_error(int(a) > 2359)
     format_error(int(str(a)[-2:]) >= 60)
     return a
+
+if __name__ == "__main__":
+    print("You found a secret message, you are not suppose to run this.")
