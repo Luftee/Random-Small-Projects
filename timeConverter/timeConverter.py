e3f62675831b7a36b846654cf1a86cc34d55e7f3
Luft <69074182+Luftee@users.noreply.github.com>
2022-05-26 09:22:23 +0900

    Update timeConverter.py

--- timeConverter/timeConverter.py
+++ timeConverter/timeConverter.py
@@ -2,11 +2,11 @@ from functions import input_check
 
 if True:
     oldZone = input_check("Original timezone: ")
-    if oldZone == False: break
+    if oldZone is False: break
     oldTime = input_check("Original time: ")
-    if oldTime == False: break
+    if oldTime is False: break
     newZone = input_check("New timezone: ")
-    if newZone == False: break
+    if newZone is False: break
         
     newTime = int(oldTime) - int(oldZone) + int(newZone)
     if int(str(newTime)[-2:]) >= 60: newTime = newTime - 60 + 100
