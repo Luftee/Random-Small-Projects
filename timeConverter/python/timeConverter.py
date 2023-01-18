da62e0d2760b032d1a87ac62f9da0eb1bdfd0701
Luft <69074182+Luftee@users.noreply.github.com>
2023-01-18 15:22:47 +0800

    Update timeConverter.py
    I have no idea what I am doing

--- timeConverter/python/timeConverter.py
+++ timeConverter/python/timeConverter.py
@@ -1,39 +1,35 @@
-def input_check(a):
-    def format_error(a):
-        if a:
-            print("\nWrong format")
-            global error; error = True
+def inputCheck(item):
+    inputStr = input(item)
 
-    b = input(a)
-    c = len(b)
+    if len(inputStr) != 4:
+        quit()
+    if int(inputStr) > 2359:
+        quit()
+    if int(str(inputStr)[-2:]) >= 60:
+        quit()
 
-    format_error(c < 3 or c > 4)
-    format_error(int(b) > 2359)
-    format_error(int(str(b)[-2:]) >= 60)
-
-    if error:
-        return False
-    else:
-        return b
+    inputArray = [int(str(inputStr)[:-2]), int(str(inputStr)[-2:])]
+    return inputArray
 
 def main():
-    oldZone = input_check("Original timezone: ")
-    if oldZone is False: return
-    oldTime = input_check("Original time: ")
-    if oldTime is False: return
-    newZone = input_check("New timezone: ")
-    if newZone is False: return
-        
-    newTime = int(oldTime) - int(oldZone) + int(newZone)
-    
-    if int(str(newTime)[-2:]) >= 60:
-        newTime = newTime - 60 + 100
-    if newTime > 2359:
-        newTime = newTime - 2400
-    
-    print("\nNew time: ", end = "")
-    if newTime < 1000: print("0", end = "")
-    print(newTime)
+    oldZone = inputCheck('Original timezone: ')
+    oldTime = inputCheck('Original time: ')
+    newZone = inputCheck('New timezone: ')
 
-if __name__ == "__main__":
+    newTimeHour = oldTime[0] - oldZone[0] + newZone[0]
+    newTimeMinute = oldTime[1] - oldZone[1] + newZone[1]
+    newTime = [newTimeHour, newTimeMinute]
+    
+    if newTime[1] >= 60:
+        newTime = [newTime[0] + 1, newTime[1] - 60]
+    if int(f'{newTime[0]}{newTime[1]}') > 2359:
+        newTime[0] = newTime[0] - 24
+
+    for i in newTime:
+        if int(newTime[i]) < 10:
+            newTime[i] = f'0{newTime[i]}'
+
+    print(f'\nNew time: {newTime[0]}{newTime[1]}')
+
+if __name__ == '__main__':
     main()
